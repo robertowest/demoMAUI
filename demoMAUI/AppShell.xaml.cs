1424c417de1727c5fcead8c9feefca997c1d5dbe
Roberto West <roberto.west@gmail.com>
2024-01-10 20:27:39 +0100

    primera carga

---
+++ demoMAUI/AppShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using demoMAUI.Code.Views;
+
+namespace demoMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // registramos todos las rutas
+            Routing.RegisterRoute("login", typeof(LoginPage));
+            Routing.RegisterRoute("main", typeof(MainPage));
+            Routing.RegisterRoute("home", typeof(HomePage));
+            Routing.RegisterRoute("settings", typeof(SettingsPage));
+        }
+    }
+}
---
+++ demoMAUI/Code/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace demoMAUI.Code.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
